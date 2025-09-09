4e5c40b06e9f622c7e91b624d4d570e01f24280e
HoffmannAdam <127486269+HoffmannAdam@users.noreply.github.com>
2025-09-09 12:25:49 +0200

    Add files via upload

--- 2025_09_09/script.js
+++ 2025_09_09/script.js
@@ -1,22 +1,44 @@
-let form = document.querySelector('form');
-let submitButton = document.querySelector("#submit");
-submitButton.addEventListener('click', (event) => {
-    event.preventDefault();
+//let form = document.querySelector('form');
+//let submitButton = document.querySelector("#submit");
+//submitButton.addEventListener('click', (event) => {
+//    event.preventDefault();
     //const data = Object.fromEntries(new FormData(form).entries());
     //console.log(data);
-    let email = document.getElementById("email").value();
-    let fname = document.getElementById("fname").value();
-    let lname = document.getElementById("lname").value();
-    let password = document.getElementById("password").value();
-    let cpassword = document.getElementById("cpassword").value();
+//    let email = document.getElementById("email").value();
+//    let fname = document.getElementById("fname").value();
+//    let lname = document.getElementById("lname").value();
+//    let password = document.getElementById("password").value();
+//    let cpassword = document.getElementById("cpassword").value();
 
-    let object = {
-        email: email,
-        firstname: fname,
-        lastname: lname,
-        password: password,
-        confirmpassword: cpassword
+//    let object = {
+//        email: email,
+//        firstname: fname,
+//        lastname: lname,
+//        password: password,
+//        confirmpassword: cpassword
+//    }
+//    console.log(object.entries())
+
+//})
+
+function crateTable()
+{
+    let table = document.createElement("table");
+    let tableBody = document.createElement("tableBody");
+
+    for (let i = 0; i < 2; i++)
+    {
+        let row = document.createElement("tr");
+
+        for (let j = 0; j < 2; i++)
+        {
+            let cell = document.createElement("td");
+            row.appendChild(cell);
+        }
+        tableBody.appendChild(row);
     }
-    console.log(object.entries())
-
-})
+    table.appendChild(tableBody);
+    document.body.appendChild(table);
+}
+document.querySelector("input[type='button']")
+        .addEventListener("click", crateTable);
