e31f38c868a642d76f5c4ac826b2976b837d483d
HoffmannAdam <127486269+HoffmannAdam@users.noreply.github.com>
2025-10-14 09:12:49 +0200

    Add files via upload

--- front_2025_10_14/main.js
+++ front_2025_10_14/main.js
@@ -1,12 +1,20 @@
-const toDo = document.querySelector("#toDo")
-const card = document.createElement("div");
-const cardBody = document.createElement("div");
-const addBtn = document.querySelector("#btn")
+const toDo = document.querySelector(".toDo");
+const addBtn = document.querySelector(".btn");
 function addCard()
 {
+    const card = document.createElement("div");
+    const cardBody = document.createElement("div");
+
     card.classList.add("card");
     cardBody.classList.add("card-body");
+
+    cardBody.textContent = document.querySelector(".input").value;
 
     card.appendChild(cardBody);
     toDo.appendChild(card);
 }
+
+addBtn.addEventListener("click", addCard)
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") addCard();
+});
